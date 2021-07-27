e12df0f8097022e349a2edb1acab9ecd0ed69c9d
Charles6489 <88054024+Charles6489@users.noreply.github.com>
2021-07-27 16:45:00 -0500

    Update codigo_post.ino

--- codigo_post/codigo_post.ino
+++ codigo_post/codigo_post.ino
@@ -12,8 +12,8 @@
 #include <WiFiClient.h> 
 #include <ESP8266WebServer.h>
 #include <ESP8266HTTPClient.h>
-const char* ssid     = "Ambafiber Orozco";
-const char* password = "0939979649";
+const char* ssid     = "wifi";
+const char* password = "clave";
 const char* host     = "192.168.1.128";
 const char* url      = "/control.php/";
 String PostData;
